1440dc2d843fd0a8eca3c0ad0f343daf7e87f0a2
Filip Wawrzyniak <wawrzyniakf8@gmail.com>
2025-01-14 02:32:59 +0100

    aint no way

--- src/js/script.js
+++ src/js/script.js
@@ -7,12 +7,12 @@ const showNavMobile = () => {
 	const computedStyle = window.getComputedStyle(navMobile)
 
 	if (computedStyle.display == 'none') {
-		burger.setAttribute('src', './dist/img/icon-close.svg')
+		burger.setAttribute('src', 'dist/img/icon-close.svg')
 		navMobile.style.display = 'block'
 		burger.style.transform = 'scale(0.9)'
 		overlay.style.display = 'block'
 	} else {
-		burger.setAttribute('src', './dist/img/icon-hamburger.svg')
+		burger.setAttribute('src', 'dist/img/icon-hamburger.svg')
 		navMobile.style.display = 'none'
 		burger.style.transform = 'scale(1)'
 		overlay.style.display = 'none'
@@ -20,9 +20,9 @@ const showNavMobile = () => {
 }
 const changeImg = () => {
 	if (innerWidth <= 768) {
-		headerImg.setAttribute('src', '/dist/img/bg-intro-mobile.svg')
-	}else if(innerWidth > 768){
-		headerImg.setAttribute('src', '/dist/img/bg-intro-desktop.svg')
+		headerImg.setAttribute('src', 'dist/img/bg-intro-mobile.svg')
+	} else if (innerWidth > 768) {
+		headerImg.setAttribute('src', 'dist/img/bg-intro-desktop.svg')
 	}
 }
 changeImg()
